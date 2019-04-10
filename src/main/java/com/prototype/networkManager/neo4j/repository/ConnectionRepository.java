f8436c09d14f3c7c3366b9369f54f25a500e422b
Aleksander Giera <aleksander.giera@protonmail.com>
2019-04-10 22:47:20 +0200

    fixed creating connections and returning new

--- src/main/java/com/prototype/networkManager/neo4j/repository/ConnectionRepository.java
+++ src/main/java/com/prototype/networkManager/neo4j/repository/ConnectionRepository.java
@@ -6,14 +6,16 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.Map;
 
 @RepositoryRestResource(exported = false)
 public interface ConnectionRepository extends PagingAndSortingRepository<Connection, Long> {
 
+    @Deprecated(forRemoval = true)
     @Query("MATCH (pM:Port)-[:IS_PORT]->(deviceMaster),(pS:Port) where ID(pM)={0} and ID(pS)={1} " +
             "CREATE (pS)-[c:CONNECTION {vlans: {2}}]->(pM) " +
             "SET pS.devicePlugged = toString([x in labels(deviceMaster) where x<>'Node' and x<>'DeviceNode'][0]) " +
             "SET pS.portOnTheUpperElement = toString(pM.portNumber) " +
             "RETURN c")
-    Connection saveConnection(Long portIdMaster, Long portIdSlave, List<String> vlans);
+    Iterable<Map<String, Object>> saveConnection(Long portIdMaster, Long portIdSlave, List<String> vlans);
 }
--- src/main/java/com/prototype/networkManager/neo4j/services/ConnectionServiceImpl.java
+++ src/main/java/com/prototype/networkManager/neo4j/services/ConnectionServiceImpl.java
@@ -1,25 +1,36 @@
 package com.prototype.networkManager.neo4j.services;
 
 import com.prototype.networkManager.neo4j.domain.Connection;
+import com.prototype.networkManager.neo4j.domain.DeviceType;
 import com.prototype.networkManager.neo4j.domain.Port;
+import com.prototype.networkManager.neo4j.exceptions.ConnectionCantCreatedException;
+import com.prototype.networkManager.neo4j.exceptions.PortNotFoundException;
 import com.prototype.networkManager.neo4j.repository.ConnectionRepository;
+import com.prototype.networkManager.neo4j.repository.DeviceNodeRepository;
 import com.prototype.networkManager.neo4j.repository.PortRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class ConnectionServiceImpl implements ConnectionService {
 
-    @Autowired
-    ConnectionRepository connectionRepository;
+    private final ConnectionRepository connectionRepository;
 
-    @Autowired
-    PortRepository portRepository;
+    private final PortRepository portRepository;
+
+    private final DeviceNodeRepository deviceNodeRepository;
+
+
+    public ConnectionServiceImpl(ConnectionRepository connectionRepository, PortRepository portRepository, DeviceNodeRepository deviceNodeRepository) {
+        this.connectionRepository = connectionRepository;
+        this.portRepository = portRepository;
+        this.deviceNodeRepository = deviceNodeRepository;
+    }
 
     @Override
     public Iterable<Connection> getConnections() {
@@ -27,16 +38,30 @@ public class ConnectionServiceImpl implements ConnectionService {
     }
 
     @Override
-    public Connection addConnection(List<Port> ports) {
+    public Connection addConnection(List<Port> ports) throws ConnectionCantCreatedException, PortNotFoundException{
         List<String> vlans = new ArrayList<>();
         vlans.add("test"); // TODO making vlans
         vlans.add("test2");
-        if(ports.get(0).getConnections()== null && ports.get(1).getConnections() == null){
-            return connectionRepository.saveConnection(ports.get(0).getId(), ports.get(1).getId(), vlans);
-        }
-        //TODO add exception
-        return new Connection();
-    }
 
+        Optional<Port> portMaster = portRepository.findById(ports.get(0).getId());
+        Optional<Port> portSlave = portRepository.findById(ports.get(1).getId());
+
+        if(portMaster.isPresent() && portSlave.isPresent()){
+            if(portMaster.get().getConnections()== null && portSlave.get().getConnections() == null){
+                Connection newConnection = new Connection();
+                portSlave.get().setPortOnTheUpperElement(Integer.toString(portMaster.get().getPortNumber()));
+                portSlave.get().setDevicePlugged(DeviceType.valueOf(deviceNodeRepository.getDeviceNodeTypeByIdOfPort(
+                        portMaster.get().getId()).get(0).get("DeviceType").toString()));
+                newConnection.setStartNode(portSlave.get());
+                newConnection.setEndNode(portMaster.get());
+                newConnection.setVlans(vlans);
+                return connectionRepository.save(newConnection);
+            } else {
+                throw new ConnectionCantCreatedException("Port/ports already occupied.");
+            }
+        } else {
+            throw new PortNotFoundException("Port with id: "+ports.get(0).getId()+" or id: "+ports.get(1).getId()+" not found.");
+        }
+    }
 
 }
